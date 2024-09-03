0821e97e1f61b7e4c1beab0f97feea64f5dbd62e
daniely <>
2024-09-03 17:56:02 +0900

    회원가입 페이지 레이아웃 완료

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def login():
     return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5050, debug=True)
 
 #sadsada

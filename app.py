31d47e4d9ae873c64662ed3f657b1f257d15cfb7
dnwls6102 <dnwls6102@gmail.com>
2024-09-03 17:59:19 +0900

    회원가입 작업중

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug = True)
 
 #sadsada
